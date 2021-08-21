aa8b0b2166ef22e4523bcb4b276954dffbb75dad
unknown <cod3girl>
2021-08-21 16:37:59 -0500

    package: stream state

--- Bootcamp_Flutter_Basico/sesion3/appsesiontres/lib/main.dart
+++ Bootcamp_Flutter_Basico/sesion3/appsesiontres/lib/main.dart
@@ -1,15 +1,46 @@
+import 'package:appsesiontres/store.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_state/stream_state.dart';
+import 'package:stream_state/stream_state_builder.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await initStreamStatePersist();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Streame state"),
+          ),
+          body: Center(
+            child: Container(
+                child: StreamStateBuilder(
+                    streamState: count,
+                    builder: (context, item) {
+                      var tempValue = "";
+
+                      if(item == 10){
+                        tempValue = "Es igual a 10";
+                      } else  tempValue = item.toString();
+                      return Text(tempValue, style: TextStyle(fontSize: 60));
+                    }            
+                  )
+                ),
+          ),
+          floatingActionButton: IconButton(
+              onPressed: () {
+                count.state++;
+                count.forceUpdate();
+              },
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ))),
     );
   }
 }
